851323c4de0c500aa2f54dafba61a896d2d4f064
karthigit47 <karthism47@gmail.com>
2026-02-04 17:42:29 -1200

    PWA setup

---
+++ src/serviceWorkerRegistration.js
@@ -0,0 +1,15 @@
+// Basic CRA service worker registration
+export function register() {
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .then(reg => {
+          console.log("Service Worker registered:", reg);
+        })
+        .catch(err => {
+          console.error("SW registration failed:", err);
+        });
+    });
+  }
+}
